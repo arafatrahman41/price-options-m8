7310920e3e9631333bc2c992625917d34ac2b621
arafatrahman41 <arafatrahman41@outlook.com>
2024-05-15 07:31:54 +0600

    start mile 8 with price options

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import DasiyNav from './components/DasiyNav/DasiyNav'
+
+function App() {
+   return (
+    <>
+      <DasiyNav></DasiyNav>
+      <h1 className='text-3xl bg-rose-500'>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
